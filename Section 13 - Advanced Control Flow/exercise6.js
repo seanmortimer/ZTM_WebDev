8f0de247563f422e8be96da966f1368cb6621fd4
Sean Mortimer <sean.mortimer@gmail.com>
2020-03-22 18:48:01 -0600

    Completed Exercise 6 of Advanced JavaScript

--- Section 13 - Advanced Control Flow/exercise6.js
+++ Section 13 - Advanced Control Flow/exercise6.js
@@ -15,12 +15,26 @@ object1.a = 4;              //4, also for 2 and 3
 // create a cow that accepts a name, type and color and has a sound method that moo's her name, type and color. 
 
 class Animal {
-    constructor(name, type) {
+    constructor(name, type, colour) {
         console.log("The animal is", this);
         this.name = name;
         this.type = type;
+        this.colour = colour;
     }
     sayHi() {
-        
+        console.log(`Hi, I am ${this.name}. I am a ${this.colour} ${this.type}!`);
     }
 }
+
+class Mammal extends Animal {
+    constructor(name, type, colour, sound) {
+        super(name, type, colour)
+        this.sound = sound;
+    }
+    sounds() {
+        console.log(`${this.sound}!!! My name is ${this.name}. I am a beautiful ${this.colour} ${this.type}!`);
+    }
+}
+
+const cow1 = new Mammal('Betsy', 'cow', 'brown', 'Moooooo')
+const cow2 = new Mammal('Brian', 'cow', 'purple', 'Mooooooieee')
