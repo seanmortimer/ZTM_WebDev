43a846be61c05ac4f7dcb89049d1506d874ff843
Sean Mortimer <sean.mortimer@gmail.com>
2020-05-06 23:18:02 -0600

    Make fun robot cards

--- 19 - React and Redux/robofriends/src/index.js
+++ 19 - React and Redux/robofriends/src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Card from './Card';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+import { robots } from './robots'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <div>
+    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
+    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
+    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
+    <Card id={robots[3].id} name={robots[3].name} email={robots[3].email} />
+    <Card id={robots[4].id} name={robots[4].name} email={robots[4].email} />
+    <Card id={robots[5].id} name={robots[5].name} email={robots[5].email} />
+  </div>
+  , document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
