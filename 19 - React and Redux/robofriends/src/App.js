d7d01560f9632ce42a9d57805eb2c24e5014b54d
Sean Mortimer <sean.mortimer@gmail.com>
2020-05-07 14:18:17 -0600

    Add search function to robots

---
+++ 19 - React and Redux/robofriends/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import CardList from './Cardlist';
+import { robots } from './robots';
+import SearchBox from "./SearchBox";
+
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+    }
+
+    render() {
+        const filteredRobots = this.state.robots.filter(robots => {
+            return robots.name.toLocaleLowerCase().includes(this.state.searchfield.toLocaleLowerCase());
+        })
+        return (
+            <div className='tc'>
+                <h1>RoboFriends!</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <CardList robots={filteredRobots} />
+            </div>
+        );
+    }
+}
+
+export default App;
